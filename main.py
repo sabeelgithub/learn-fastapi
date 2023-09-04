d8c2f60b221c2ff0ac94bf3542515825a249023f
mohammed sabeel <mohammedsabeeltc786@gmail.com>
2023-09-04 18:24:42 +0530

    working on it

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,3 +11,27 @@ def index():
 @app.get('/about')
 def about():
     return {'data':'about page'}
+
+@app.get("/blog")
+def blog():
+    return {"data":"blog list"}
+
+@app.get("/blogs")
+def blog(published:bool,limit:int=10):
+    if published:
+      return {"data":f'{limit} blogs'}
+    return {"data":'all blogs'}
+
+@app.get('/blog/unpublished')
+def show():
+    return {'data':'unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}
+
