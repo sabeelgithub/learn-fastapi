a700ef301914eaa3c0b20a7b0139206c044ce6a3
mohammed sabeel <mohammedsabeeltc786@gmail.com>
2023-09-05 18:17:20 +0530

    woirking on post method

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import re
+from urllib import request
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -17,7 +21,7 @@ def blog():
     return {"data":"blog list"}
 
 @app.get("/blogs")
-def blog(published:bool,limit:int=10):
+def blog(published:bool=True,limit:int=10,sort:Optional[str]=None):
     if published:
       return {"data":f'{limit} blogs'}
     return {"data":'all blogs'}
@@ -34,4 +38,20 @@ def show(id:int):
 @app.get('/blog/{id}/comments')
 def comments(id):
     return {'data':{'1','2'}}
+
+ 
+
+@app.post('/blog')
+def checkhing():
+    return {'data':'this is post method'}
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published_at : Optional[bool]
+
+
+@app.post('/blogs')
+def create_blog(request:Blog):
+    return f'blog created with title {request.title}'
 
