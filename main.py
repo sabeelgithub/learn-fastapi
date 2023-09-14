5ac302c1bf6b3400b0209c7d4fafbbe0e8aee850
mohammed sabeel <mohammedsabeeltc786@gmail.com>
2023-09-14 16:55:33 +0530

    blog folder created

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import re
-from urllib import request
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-
+import uvicorn
 
 app = FastAPI()
 
@@ -52,6 +50,9 @@ class Blog(BaseModel):
 
 
 @app.post('/blogs')
-def create_blog(request:Blog):
-    return f'blog created with title {request.title}'
+def create_blog(blog:Blog):
+    return f'blog created with title {blog.title}'
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
 
